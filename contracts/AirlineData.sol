590412ae24a8344979e011e0f75237bb0ee4f26d
Kaicoh <sumireminami@gmail.com>
2019-05-12 15:59:41 +0900

    add isRegistered modifier

--- contracts/AirlineData.sol
+++ contracts/AirlineData.sol
@@ -3,28 +3,53 @@ pragma solidity ^0.5.2;
 contract AirlineData {
     mapping (address => Airline) private airlines;
 
+    uint public registeredCount = 0;
+    uint private constant CAN_REGISTER_WITHOUT_CONSENSUS = 4;
+
     struct Airline {
         string name;
-        bool active;
+        Status status;
+        uint deposit;
+    }
+
+    enum Status {
+        NotRegistered,
+        Registered,
+        Participating
+    }
+
+    modifier isRegistered() {
+        require(
+            airlines[msg.sender].status != Status.NotRegistered,
+            "Only registered airline is allowed to execute"
+        );
+        _;
     }
 
     constructor(string memory _name) public {
         // spec: First airline is registered when contract is deployed.
-        airlines[msg.sender] = Airline({
-            name: _name,
-            active: true
-        });
+        _addAirline(_name, msg.sender);
     }
 
-    function fetchAirline(address _address)
+    function registerAirline(string memory _name, address _address)
         public
-        view
-        returns(
-            string memory name,
-            bool active
-        )
+        isRegistered
     {
-        name = airlines[_address].name;
-        active = airlines[_address].active;
+        // spec: Only existing airline may register a new airline until there are at least four airlines registered
+        if (registeredCount < CAN_REGISTER_WITHOUT_CONSENSUS) {
+            _addAirline(_name, _address);
+        }
+    }
+
+    function _addAirline(string memory _name, address _address) private {
+        require(airlines[_address].status == Status.NotRegistered, "Already registered");
+
+        airlines[_address] = Airline({
+            name: _name,
+            status: Status.Registered,
+            deposit: 0
+        });
+
+        registeredCount++;
     }
 }
--- test/TestAirlineData.js
+++ test/TestAirlineData.js
@@ -3,13 +3,47 @@ const AirlineData = artifacts.require('AirlineData');
 contract('AirlineData', (accounts) => {
     let instance;
 
+    let airline1;
+    let airline2;
+    let airline3;
+    let airline4;
+    let airline5;
+
+    before(() => {
+        /* eslint-disable prefer-destructuring */
+        airline1 = accounts[0];
+        airline2 = accounts[1];
+        airline3 = accounts[2];
+        airline4 = accounts[3];
+        airline5 = accounts[4];
+        /* eslint-enable prefer-destructuring */
+    });
+
     beforeEach(async () => {
         instance = await AirlineData.deployed();
     });
 
     it('registers first airline when deployed', async () => {
-        const airline = await instance.fetchAirline.call(accounts[0]);
-        assert.notStrictEqual(airline.name, '');
-        assert.equal(airline.active, true);
+        const registeredCount = await instance.registeredCount.call();
+        assert.equal(registeredCount.toNumber(), 1);
     });
+
+    it('can not register a new airline when requested by not registered airline', async () => {
+        try {
+            await instance.registerAirline('some airline', airline2, { from: airline3 });
+            throw new Error('unreachable error');
+        } catch (error) {
+            assert.match(
+                error.message,
+                /Only registered airline is allowed to execute/,
+                'Not registered can register a new airline',
+            );
+        }
+    });
+
+    // describe('registers a new airline', () => {
+    //     describe('without consensus when registered count is less than 4');
+
+    //     describe('with consensus when registered count is greater than or equal to 4');
+    // });
 });
