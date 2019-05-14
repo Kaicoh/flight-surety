acdfc425724a79c6be37a8cfeaf14a767772ee23
Kaicoh <sumireminami@gmail.com>
2019-05-14 15:25:11 +0900

    fix test environment

--- truffle.js
+++ truffle.js
@@ -1,9 +1,13 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+
 module.exports = {
     networks: {
         development: {
-            host: '127.0.0.1',
-            port: 8545,
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, 'http://127.0.0.1:8545/', 0, 50),
             network_id: '*',
+            gas: 6700000,
         },
     },
 
